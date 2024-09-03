0bf44f6855afbf814cc5f743be3d580909d08cc6
codeeraayush <aayushpandey1100@gmail.com>
2024-09-03 18:03:23 +0530

    Initial Commit

---
+++ FirstApp-SwiftUi/FirstApp_SwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstApp_SwiftUiApp.swift
+//  FirstApp-SwiftUi
+//
+//  Created by Aayush Pandey on 03/09/24.
+//
+
+import SwiftUI
+
+@main
+struct FirstApp_SwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
